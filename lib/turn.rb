1387f1599384edcc7d9858620a3d76d53cc2a55a
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-09 23:04:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,28 @@ def position_taken?(board, index)
     return true 
   end 
 end 
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(num_str)
+  valid_str_arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  num_str = num_str.strip
+  
+  if valid_str_arr.include?(num_str)
+    return num_str.to_i - 1 
+  else
+    return -1
+  end
+end
+
+def move(board, player_move_idx, player_char="X")
+  # player_move_idx = input_to_index(player_move_idx)
+  board[player_move_idx] = player_char
+  return board
+end
